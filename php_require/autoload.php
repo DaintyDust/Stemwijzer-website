308515ef69cde15d67edf74c0ef7b38f5adffea4
DaintyDust <nick-verbruggen@hotmail.com>
2025-06-30 19:45:43 +0200

    Remove HTML output from autoload.php to prevent resources appearing outside head tag

--- php_require/autoload.php
+++ php_require/autoload.php
@@ -13,6 +13,3 @@ if (!function_exists('session_start')) {
 require 'private/database.php';
 require 'private/checklogin.php';
 ?>
-
-<link rel="stylesheet" href="styles/main.css">
-<script src="js/main.js"></script>
